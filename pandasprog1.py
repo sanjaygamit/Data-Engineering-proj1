838c9d3dd0ee0029a41c182ac43e2f6c7f3a479f
sanjay gamit <sanjugamit@gmail.com>
2025-05-15 11:40:11 +0530

    file added

--- pandasprog1.py
+++ pandasprog1.py
@@ -1,28 +1,28 @@
 import pandas as pd
 import json
 
-file_path = 'data-engineering-proj1/retail_db/schemas.json'
-schemas = json.load(open(file_path, 'r'))
-def get_column_names(schemas, ds_name, sorting_key = 'column_position'):
-    column_details = schemas[ds_name]
-    columns = sorted(column_details, key = lambda col: col[sorting_key])
-    return [col['column_name'] for col in columns]
+# file_path = 'data-engineering-proj1/retail_db/schemas.json'
+# schemas = json.load(open(file_path, 'r'))
+# def get_column_names(schemas, ds_name, sorting_key = 'column_position'):
+#     column_details = schemas[ds_name]
+#     columns = sorted(column_details, key = lambda col: col[sorting_key])
+#     return [col['column_name'] for col in columns]
 
-departments_col = get_column_names(schemas, 'departments', 'column_position')
-categories_col = get_column_names(schemas, 'categories', 'column_position')
-orders_col = get_column_names(schemas, 'orders', 'column_position')
-products_col = get_column_names(schemas, 'products', 'column_position')
-customers_col = get_column_names(schemas, 'customers', 'column_position')
-order_items_col = get_column_names(schemas, 'order_items', 'column_position')
+# departments_col = get_column_names(schemas, 'departments', 'column_position')
+# categories_col = get_column_names(schemas, 'categories', 'column_position')
+# orders_col = get_column_names(schemas, 'orders', 'column_position')
+# products_col = get_column_names(schemas, 'products', 'column_position')
+# customers_col = get_column_names(schemas, 'customers', 'column_position')
+# order_items_col = get_column_names(schemas, 'order_items', 'column_position')
 
 # print(order_items_col)
 
-categories = pd.read_csv('Data-Engineering-proj1/retail_db/categories/part-00000', header=None, names=categories_col)
-customers = pd.read_csv('Data-Engineering-proj1/retail_db/customers/part-00000', header=None, names=customers_col)
-departments = pd.read_csv('Data-Engineering-proj1/retail_db/departments/part-00000', header=None, names=departments_col)
-order_items = pd.read_csv('Data-Engineering-proj1/retail_db/order_items/part-00000', header=None, names=order_items_col)
-orders = pd.read_csv('Data-Engineering-proj1/retail_db/orders/part-00000', header=None, names=orders_col)
-products = pd.read_csv('Data-Engineering-proj1/retail_db/products/part-00000', header=None, names=products_col)
+# categories = pd.read_csv('Data-Engineering-proj1/retail_db/categories/part-00000', header=None, names=categories_col)
+# customers = pd.read_csv('Data-Engineering-proj1/retail_db/customers/part-00000', header=None, names=customers_col)
+# departments = pd.read_csv('Data-Engineering-proj1/retail_db/departments/part-00000', header=None, names=departments_col)
+# order_items = pd.read_csv('Data-Engineering-proj1/retail_db/order_items/part-00000', header=None, names=order_items_col)
+# orders = pd.read_csv('Data-Engineering-proj1/retail_db/orders/part-00000', header=None, names=orders_col)
+# products = pd.read_csv('Data-Engineering-proj1/retail_db/products/part-00000', header=None, names=products_col)
 
 # print(orders['order_status'])
 # print(orders['order_status'].unique())
@@ -62,8 +62,8 @@ products = pd.read_csv('Data-Engineering-proj1/retail_db/products/part-00000', h
 
 import glob
 file_path = glob.glob('Data-Engineering-proj1/retail_db/**', recursive=True)
-for f in file_path:
-    print(f)
+print(file_path)
+
 
 
 
