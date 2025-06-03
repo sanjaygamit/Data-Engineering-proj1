87b85f7d4c270a68a7a5ff55e10a8806ed442d5d
sanjay gamit <sanjugamit@gmail.com>
2025-06-03 15:06:52 +0530

    file added

--- Pythonprogtester.py
+++ Pythonprogtester.py
@@ -1,5 +1,16 @@
+import json
+import os
 
 
+def process_files(ds_names = None):
+    src_base_dir = os.environ.get('SRC_BASE_DIR')
+    tgt_base_dir = os.environ.get('TGT_BASE_DIR')
+    schemas = json.load(open(f'{src_base_dir}/schemas.json'))
+    if not ds_names:
+        ds_names = schemas.keys()
+    for ds_name in ds_names:
+        print(f'Processing dataset: {ds_name}')
+    
 
 
 
