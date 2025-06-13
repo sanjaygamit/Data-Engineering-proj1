b91135aa8b00b22aa4e21a837c18eb3056567344
sanjay gamit <sanjugamit@gmail.com>
2025-06-13 12:45:59 +0530

    file modified PysaprPractice.py

--- PysaprPractice.py
+++ PysaprPractice.py
@@ -3,6 +3,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, sum as _sum, desc # Alias sum to avoid conflict
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType
 from pyspark.sql.functions import explode, split
+from pyspark.sql.functions import * 
 
 # Write a pyspark query  using below input to get below output.
 
@@ -58,4 +59,13 @@ columns = ["city1","city2","city3"]
 spark = SparkSession.builder.appName("CitySplit").getOrCreate()
 
 df = spark.createDataFrame(data,columns)
-df.show()
+# df.show()
+
+# df1 = df.withColumn('FirstNotNull',coalesce(df.city1,df.city2,df.city3))
+df1 = df.withColumn('FirstNotNull',coalesce(
+    when(df.city1 == '', None).otherwise(df.city1),
+    when(df.city2 == '',None).otherwise(df.city2),
+    when(df.city3 == '',None).otherwise(df.city3)) )
+
+df3 = df1.select(df1.FirstNotNull)
+df3.show()
