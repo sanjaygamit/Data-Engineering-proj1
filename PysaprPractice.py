6eeb7c1c35292c6a2e2d0663d79e75b19e719fe0
sanjay gamit <sanjugamit@gmail.com>
2025-06-13 11:24:42 +0530

    file added PysaprPractice.py

---
+++ PysaprPractice.py
@@ -0,0 +1,26 @@
+import json
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, sum as _sum, desc # Alias sum to avoid conflict
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType
+
+# Write a pyspark query  using below input to get below output.
+
+# # input : 
+# name  | Hobbies           |
+# --------------------------|
+# Alice | Badminton, Tennis |
+# Bob   | Tennis, Cricket   |
+# Julie | Cricket, Carroms  | 
+
+
+# # output 
+
+# Name | Hobbies 
+# Alice | Badminton
+# Alice | Tennis
+# Bob   | Tennis
+# Bob   | Cricket
+# Julie | Cricket
+# Julie | Carroms
+
+
