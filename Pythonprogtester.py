5b4e63d1ec638b0ac8986b821629d49d1506160b
sanjay gamit <sanjugamit@gmail.com>
2025-06-13 10:13:04 +0530

    modified Pythonprogtester.py

--- Pythonprogtester.py
+++ Pythonprogtester.py
@@ -2,7 +2,11 @@ import logging
 
 logging.basicConfig(level=logging.INFO, filename="log.log", filemode="w")
 
-
+logging.debug("debug")
+logging.info("info")
+logging.warning("warning")
+logging.error("error")
+logging.critical("critical")
 
 import json
 import os
