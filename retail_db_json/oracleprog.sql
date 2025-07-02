7cecd28bc75cc5f64b6eea31599bbbaf14dac111
sanjay gamit <sanjugamit@gmail.com>
2025-07-02 13:11:16 +0530

    file modified

--- retail_db_json/oracleprog.sql
+++ retail_db_json/oracleprog.sql
@@ -594,3 +594,27 @@ from emp_t a
 
 
 
+# EXCEPTION 
+
+    access_into_null
+    case_not_found
+    collection_is_null
+    cursor_already_open
+    dup_val_on_index
+    invalid_cursor
+    invalid_number
+    login_denied
+    no_data_found
+    not_logged_on
+    program_error
+    rowtype_mismatch
+    self_is_null
+    storage_error
+    subscript_beyond_count
+    subscript_outside_limit
+    sys_invalid_rowid
+    timeout_on_resource
+    too_many_rows
+    value_error
+    zero_divide
+    
