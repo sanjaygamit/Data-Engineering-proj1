483b7fe8661a0f42a85ea9fcc941829dc35d28e7
sanjay gamit <sanjugamit@gmail.com>
2025-08-07 12:27:51 +0530

    file modified

--- PysaprPractice.py
+++ PysaprPractice.py
@@ -399,9 +399,9 @@ df = spark.createDataFrame(data, schema)
 
 df1 = spark.createDataFrame(data1, schema1)
 
-df1.select("name",df1.skills,explode(df1.skills).alias("skill")).show()
-# df1.select("name",df1.skills,posexplode(df1.skills).alias("skill")).show()
-df1.select("name",df1.skills,explode_outer(df1.skills).alias('ex_skill')).show()
+# df1.select("name",df1.skills,explode(df1.skills).alias("skill")).show()
+df1.select("name",df1.skills,posexplode(df1.skills)).show()
+# df1.select("name",df1.skills,explode_outer(df1.skills).alias('ex_skill')).show()
 
 
 # df.select("Country", explode("States").alias("State", "Capital")).show()
